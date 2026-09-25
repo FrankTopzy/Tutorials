eef357ca3b697c16802addf47f8051b6aaa35cdf
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-09-26 00:11:10 +0100

    feat: task 3 completed

---
+++ task3.js
@@ -0,0 +1,56 @@
+async function getGithubUser(username) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}`
+    );
+
+    if (!response.ok) {
+      console.log("status:", response.status);
+      console.log(
+        "remaining:",
+        response.headers.get("x-ratelimit-remaining")
+      );
+      console.log(
+        "reset:",
+        response.headers.get("x-ratelimit-reset")
+      );
+      throw new Error(`Request failed: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+getGithubUser("octocat").then(user => {
+  console.log(user);
+}).catch(error => console.log("Couldn't get user:", error.message))
+
+const state = {
+  loading: false,
+  user: null,
+  error: null
+};
+
+async function searchUser(username) {
+  state.user = null;
+  state.error = null;
+  state.loading = true;
+
+  try {
+    const user = await getGithubUser(username);
+    state.user = user;
+  } catch (error) {
+    state.error = error.message;
+  } finally {
+    state.loading = false;
+  }
+
+  console.log(state);
+}
+
+searchUser('')
